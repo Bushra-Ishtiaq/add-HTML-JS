937ad5e501fb800968b2fff4ffa98eb2ff7500c7
Bushra Ishtiaq <62174610+Bushra-Ishtiaq@users.noreply.github.com>
2020-05-05 20:55:11 +0500

    added a file

---
+++ script.js
@@ -0,0 +1,9 @@
+function myName() {
+    document.querySelector ('span').innerHTML = document.querySelector('input').value;
+}
+function changeColor() {
+    document.querySelector('h1').style = 'color: green';
+}
+function clearColor() {
+    document.querySelector('h1').style = 'color: black';
+}
